ca914fdbcf51df4fcd51dd75945f3b5aec9f47e6
James Skön <skonjp@kenyon.edu>
2024-09-06 16:35:41 -0400

    Update Rectangle.cpp

--- Rectangle.cpp
+++ Rectangle.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+using namespace std;
 
 class Rectangle {
 private:
@@ -9,18 +10,40 @@ public:
     // Constructor
     Rectangle(int l, int w) : length(l), width(w) {}
 
-    // Function to calculate area
+    // Function to calculate the area of the rectangle
     int area() const {
         return length * width;
     }
 
     // Function to display the dimensions
     void display() const {
-        std::cout << "Rectangle with length " << length << " and width " << width << std::endl;
+        cout << "Rectangle with length " << length << " and width " << width << endl;
     }
 
-    // Destructor (optional for demonstration)
+    // Destructor
     ~Rectangle() {
-        std::cout << "Rectangle destroyed!" << std::endl;
+        cout << "Rectangle destroyed!" << endl;
     }
 };
+
+int main() {
+    // Dynamically allocate memory for a Rectangle object using a pointer
+    Rectangle* rectPtr = new Rectangle(10, 5);
+
+    // Access members using the pointer (-> operator)
+    rectPtr->display();
+    cout << "Area of the rectangle: " << rectPtr->area() << endl;
+
+    // Dynamically allocate memory for another Rectangle
+    Rectangle* anotherRectPtr = new Rectangle(15, 7);
+
+    // Access the new Rectangle's data
+    anotherRectPtr->display();
+    cout << "Area of the second rectangle: " << anotherRectPtr->area() << endl;
+
+    // Free the dynamically allocated memory
+    delete rectPtr;
+    delete anotherRectPtr;
+
+    return 0;
+}
